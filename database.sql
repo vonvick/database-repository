ecee765a637a3c2d8c5c41e936db716b404a21ef
vonvick <victor.nwaiwu9@gmail.com>
2016-10-24 15:29:05 +0100

    a users table query that creates the users table

--- database.sql
+++ database.sql
@@ -1,3 +1,14 @@
 // create the database
 
 CREATE DATABASE bookievault;
+
+CREATE TABLE users (
+  id int AUTO_INCREMENT,
+  firstname varchar(255) NOT NULL,
+  lastname varchar(255) NOT NULL,
+  email varchar(255) NOT NULL UNIQUE,
+  password varchar(255) NOT NULL,
+  role varchar(255) NOT NULL DEFAULT 'user',
+  imagepath varchar(255) NOT NULL,
+  PRIMARY KEY (Id)
+)
